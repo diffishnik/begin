bf9e23e8e2f6045ffdc273b3c4f4df8806769ae4
diffishnik <150814225+diffishnik@users.noreply.github.com>
2023-11-28 18:22:36 +0500

    дз

--- dancho.py
+++ dancho.py
@@ -2,6 +2,7 @@ s = input().split()
 a = int(s[0])
 b = int(s[1])
 c = int(s[2])
-s = a*b*c
-p = 2*(a*b+b*c+a*c)
-print(f"{int(p)} {int(s)}")
+if a+b > c and a+c > b and b+c > a:
+    print("YES")
+else:
+    print("NO")
