e2bc870ecc7ec3468471fac8177c41a09cbb6386
diffishnik <150814225+diffishnik@users.noreply.github.com>
2024-01-06 15:38:41 +0500

    Add files via upload

--- dancho.py
+++ dancho.py
@@ -1,8 +1,15 @@
-s = input().split()
-a = int(s[0])
-b = int(s[1])
-c = int(s[2])
-if a+b > c and a+c > b and b+c > a:
-    print("YES")
-else:
-    print("NO")
+#Напишите программу, которая принимает на вход список чисел и выводит только уникальные числа, упорядоченные по возрастанию.
+def unique_sorted_numbers(input_list):
+
+    unique_numbers_set = set(input_list)
+
+
+    sorted_unique_numbers = sorted(list(unique_numbers_set))
+
+    return sorted_unique_numbers
+
+
+input_numbers = [5, 2, 8, 2, 1, 5, 9, 8, 3]
+result = unique_sorted_numbers(input_numbers)
+
+print("Уникальные числа по возрастанию:", result)
