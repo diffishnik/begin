c68b0d51293ffa3fc5ab2dead7c8dd001a712698
diffishnik <150814225+diffishnik@users.noreply.github.com>
2024-01-06 16:21:00 +0500

    Add files via upload

---
+++ hz2.py
@@ -0,0 +1,8 @@
+#Напишите программу, которая принимает на вход число и выводит его квадратный корень.
+def koren(ss):
+    return ss * ss
+
+chislo = int(input("Vvedite chislo: "))
+otvet =  koren(chislo)
+print(otvet)
+
